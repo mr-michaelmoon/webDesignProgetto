fe9889ca73d1d8b73558a2b1ae0d050019c047e8
carlo <carlozecca1@gmail.com>
2024-06-09 13:09:53 +0200

    aggiunto sfondo menu, correzione homepage/banner ma ancora problemi

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ document.addEventListener('DOMContentLoaded', function() {
     var homepage = document.getElementById('homepage');
     var banner = document.getElementById('banner');
     var testo1 = document.getElementById('testo1');
-
+    var menu = document.getElementById('menu');
+    var viewportHeight = window.innerHeight;
+    
     document.addEventListener('scroll', function(e) {
         var homepageBounding = homepage.getBoundingClientRect();
         var bannerBounding = banner.getBoundingClientRect();
 
 
-        if(bannerBounding.top <= 0) {
-            testo1.style.animation = "fadeIn 2s forwards";
+        if(isInTheViewport(testo1)) {
+            testo1.style.animation = "fadeIn 3s forwards";
         }
+        if(testo1.getBoundingClientRect().bottom >= viewportHeight) {
+            testo1.style.animation = "fadeOut 3s forwards";
+        }
+
+
     
     });
 });
+
+function isInTheViewport(element) {
+    return element.getBoundingClientRect().top >= 0 && element.getBoundingClientRect().bottom <= window.innerHeight;
+}
