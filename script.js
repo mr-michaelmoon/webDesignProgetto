b7c6ffa8af5c9bc479eaa928f303b5270f18f837
carlo <carlozecca1@gmail.com>
2024-06-15 11:53:17 +0200

    aggiustato posizionamento uccello, homepage e i frame del banner

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-
+const numFrames = 32;
 document.addEventListener('DOMContentLoaded', function() {
 
     var homepage = document.getElementById('homepage');
     var banner = document.getElementById('banner');
+    var bannerAltezza = document.getElementById('bannerAltezza');
     var insiemeFramesBanner = document.getElementById('insiemeFramesBanner');
+    var frameBanner = document.getElementsByClassName('frameBanner');
+    var birdContainer = document.getElementById('birdContainer');
+    var bird = document.getElementById('uccelloSeduto');
     var testo1 = document.getElementById('testo1');
     var menu = document.getElementById('menu');
     var viewportHeight = window.innerHeight;
+    var viewportWidth = window.innerWidth;
     banner.style.height = window.screen.height + "px";
     banner.style.width = window.screen.width + "px";
     homepage.style.width = window.screen.width + "px";
 
     // creazione frame delle immagini del banner 
-    for(var i = 2; i< 32; i++) {
-        insiemeFramesBanner.innerHTML += "<div class='frameBanner'><img src='images/framesBanner/frame-" + i + ".png' /></div>";
+    for(var i = 2; i< numFrames; i++) {
+        insiemeFramesBanner.innerHTML += "<div class='frameBanner'><img draggable='false' src='images/framesBanner/frame-" + i + ".png' /></div>";
+        
     }
+    
+
+    window.onload = function() {
+        var bannerWidth = banner.offsetWidth;
+        bannerAltezza.style.height = numFrames * viewportWidth  + "px";
+        birdContainer.style.width = window.screen.width + "px";
+    }
+
+    window.onresize = function() {
+        var bannerWidth = banner.offsetWidth;
+        bannerAltezza.style.height = bannerWidth + "px";
+    }
+
+
     
     document.addEventListener('scroll', function(e) {
         var homepageBounding = homepage.getBoundingClientRect();
