261626411aaa4e5bd162f3748143aeceaa73010f
carlo <carlozecca1@gmail.com>
2024-06-16 11:51:22 +0200

    animazione volo rondine

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var bannerContainer = document.getElementById('bannerContainer');
     var insiemeFramesBanner = document.getElementById('insiemeFramesBanner');
     var frameBanner = document.getElementsByClassName('frameBanner');
+    var sittingBirdContainer = document.getElementById('sittingBirdContainer');
+    var sittingBird = document.getElementById('uccelloSeduto');
     var birdContainer = document.getElementById('birdContainer');
-    var bird = document.getElementById('uccelloSeduto');
+    var birdImgStory = document.getElementById('birdImgStory');
     var testo1 = document.getElementById('testo1');
     var menu = document.getElementById('menu');
     var viewportHeight = window.innerHeight;
@@ -28,8 +30,8 @@ document.addEventListener('DOMContentLoaded', function () {
     window.onload = function () {
         viewportWidth = window.innerWidth;
         bannerContainer.style.height = (numFrames - 2) * window.screen.width - viewportWidth + "px";
-        birdContainer.style.width = window.screen.width + "px";
-        menuAnimation(bird.getBoundingClientRect().top, menu, viewportHeight)
+        sittingBirdContainer.style.width = window.screen.width + "px";
+        menuAnimation(sittingBird.getBoundingClientRect().top, menu, viewportHeight)
     }
     //si attiva quando si modifica la dimensione della finestra
     window.onresize = function () {
@@ -37,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var vh = Math.abs(viewportHeight - window.innerHeight); // calcolo differenza tra viewportHeight vecchio e quello nuovo
         // window.alert();
         bannerContainer.style.height = (numFrames - 2) * window.screen.width - viewportWidth + vh + "px";
-        birdContainer.style.width = window.screen.width + "px";
+        sittingBirdContainer.style.width = window.screen.width + "px";
         banner.style.top = "50%";
             banner.style.transform = "translateY(-50%)";
 
@@ -48,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var homepageBounding = homepage.getBoundingClientRect();
         var bannerBounding = banner.getBoundingClientRect();
         var bannerContainerBounding = bannerContainer.getBoundingClientRect();
-        var birdBounding = bird.getBoundingClientRect();
+        var sittingBirdBounding = sittingBird.getBoundingClientRect();
         var scrollTop = window.scrollY || document.documentElement.scrollTop;
         scrollDirection = scrollTop > lastScrollTop ? 1 : -1;
 
@@ -68,30 +70,32 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
 
+        if(bannerContainerBounding.top <= viewportHeight && bannerContainerBounding.bottom >= viewportHeight) {
+            if (sittingBirdBounding.top <= viewportHeight / 2) {
+                if(banner.style.position != "fixed")
+                    banner.style.left = "0";
+                
+                banner.style.position = "fixed";
+                banner.style.top = "50%";
+                banner.style.transform = "translateY(-50%)";
 
-        if (birdBounding.top <= viewportHeight / 2) {
-            if(banner.style.position != "fixed")
-                banner.style.left = "0";
-            
-            banner.style.position = "fixed";
-            
-            banner.style.top = "50%";
-            banner.style.transform = "translateY(-50%)";
+                
+                if (bannerContainerBounding.top >= 0) {
+                    banner.style.position = "relative";
+                    banner.style.left = "0";
+                    banner.style.top = "0";
+                    banner.style.transform = "translateY(0)";
+                }
 
-
-            if (bannerContainerBounding.top >= 0) {
-                banner.style.position = "relative";
-                banner.style.left = "0";
-                banner.style.top = "0";
-                banner.style.transform = "translateY(0)";
             }
+
+            birdAnimation(bannerBounding.left, sittingBird, birdContainer, birdImgStory, scrollTop, viewportWidth);
 
         }
 
+        menuAnimation(sittingBirdBounding.top, menu, viewportHeight);
 
-        menuAnimation(birdBounding.top, menu, viewportHeight);
-
-            horizontalScroll(bannerContainerBounding, banner, viewportHeight)
+        horizontalScroll(bannerContainerBounding, banner, viewportHeight)
 
     });
 });
@@ -126,5 +130,38 @@ function horizontalScroll(bannerContainerBounding, banner, viewportHeight) {
         if(Math.abs(banner.style.left) >= bannerContainer.offsetHeight) {
             banner.style.left = bannerContainer.offsetHeight + "px";
         }
+    }
+}
+
+function birdAnimation(bannerBoundingLeft, sittingBird, birdContainer, birdImgStory, scrollTop, viewportWidth) {
+    let halfViewportWidth = viewportWidth / 2;
+    if(Math.abs(bannerBoundingLeft) >= halfViewportWidth) {
+        sittingBird.style.display = "none";
+        birdContainer.style.display = "block";
+        const value = 50; // quantita di scroll per cambiare uccello
+        let bannerContainerDividedValue = bannerContainer.offsetHeight / value;
+ 
+
+        //quando si ha scrollato per un tot di quantita e il primo tot non conta
+        if(Math.abs(scrollTop - lastScrollTop) >= bannerContainerDividedValue ) {
+            
+                if(birdImgStory.src.includes("AliSu") || birdImgStory.src.includes("Alzata")) {
+                    birdImgStory.src = "images/rondine/rondineWebp/rondineAliGiu.webp";
+                } else {
+                    if(birdImgStory.src.includes("AliGiu")) {
+                        birdImgStory.src = "images/rondine/rondineWebp/rondineAliSu.webp";
+                    }
+                }
+                lastScrollTop = scrollTop;
+            
+        }
+        if(Math.abs(bannerBoundingLeft) >= halfViewportWidth && Math.abs(bannerBoundingLeft) <= bannerContainerDividedValue) {
+            birdImgStory.src = "images/rondine/rondineWebp/rondineAlzata.webp";
+        }
+        
+
+    } else {
+        sittingBird.style.display = "block";
+        birdContainer.style.display = "none";
     }
 }
