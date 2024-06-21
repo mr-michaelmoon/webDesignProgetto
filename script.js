4b463dc2a67d6d19672d466d6de05f8746ec2ab7
carloz1310 <carlozecca1@gmail.com>
2024-06-21 11:51:11 +0200

    formattazione testo storia

--- script.js
+++ script.js
@@ -202,28 +202,28 @@ function calcTotalWidthFrames() {
 
 //restituisce array con i testi della storia
 function allTexts() {
-    return ["Una rondine viveva felice in un piccolo nido in Kenya.<br> Ogni giorno, volava libera sotto il cielo azzurro, sorvolando le verdi savane e i fiumi scintillanti.<br>Ma con l\'arrivo dell\'autunno, la rondine sent<span class='charSpecial'>ì</span> un forte desiderio di partire per un lungo viaggio,<br>un viaggio che l\'avrebbe portata fino all\'Italia.",
-            "Un mattino presto, la rondine spieg<span class='charSpecial'>ò</span> le ali e inizi<span class='charSpecial'>ò</span> il suo volo verso nord.",
-            "Il primo tratto del viaggio fu piacevole e pieno di avventure, ma presto arriv<span class='charSpecial'>ò</span> davanti a una sfida imponente…",
+    return ["Una rondine viveva felice in un piccolo nido in Kenya. <br>Ogni giorno, volava libera sotto il cielo azzurro, <br>sorvolando le verdi savane e i fiumi scintillanti. <br>Ma con l\'arrivo dell\'autunno, la rondine sent<span class='charSpecial'>ì</span> <br>un forte desiderio di partire per un lungo viaggio, <br>un viaggio che l\'avrebbe portata fino all\'Italia.",
+            "Un mattino presto, la rondine spieg<span class='charSpecial'>ò</span> le ali <br>e inizi<span class='charSpecial'>ò</span> il suo volo verso nord.",
+            "Il primo tratto del viaggio fu <br>piacevole e pieno di avventure,<br>ma presto arriv<span class='charSpecial'>ò</span> davanti a<br>una sfida imponente…",
             "…Il deserto del Sahara.",
             "La vastità del deserto era infinita e il sole bruciava alto nel cielo.",
-            "\"Oh, cara rondine spero che tu abbia abbastanza forza per attraversare questo deserto spietato.\"",
-            "La rondine vol<span class='charSpecial'>ò</span> senza sosta,<br>sentendo il calore ardente sotto le sue ali.<br>La sabbia sembrava non finire mai,<br>ma la piccola rondine non si arrese.",
-            "Beveva dalle rare oasi che incontrava e riposava sugli arbusti sparsi qua e là.",
+            "\"Oh, cara rondine spero che tu abbia abbastanza <br>forza per attraversare questo deserto spietato.\"",
+            "La rondine vol<span class='charSpecial'>ò</span> senza sosta, <br>sentendo il calore ardente sotto le sue ali.<br>La sabbia sembrava non finire mai,<br>ma la piccola rondine non si arrese.",
+            "Beveva dalle rare oasi che incontrava <br>e riposava sugli arbusti sparsi qua e là.",
             "Dopo giorni e giorni di volo, finalmente vide all'orizzonte qualcosa di diverso!",
             "Il blu scintillante del Mar Mediterraneo.",
             "<span class='charSpecial'>\“</span>Ben fatto! Hai superato il Sahara! Ora ti aspetta un'altra sfida:<br>lo Stretto di Gibilterra.<span class='charSpecial'>\”</span>",
-            "Lo Stretto di Gibilterra era un passaggio stretto e ventoso<br>che separava l'Africa dall'Europa. Le correnti erano forti<br>e la rondine dovette lottare contro i venti impetuosi.<br>Attraversare lo Stretto era una delle parti pi<span class='charSpecial'>ù</span> difficili del suo viaggio,<br>ma la vista della terra dall<span class='charSpecial'>’</span>altra parte le dava la forza necessaria.",
-            "Con grande determinazione,<br>raggiunse finalmente le coste spagnole.",
-            "Dalla Spagna, la rondine vol<span class='charSpecial'>ò</span> verso i maestosi Monti Pirenei. Qui, i boschi erano fitti e verdi, pieni di alberi alti e profumati. Era un luogo meraviglioso, ma il volo tra le montagne non era semplice. Le correnti d'aria potevano essere imprevedibili,<br>e a volte il cielo si copriva di nubi minacciose.",
-            "<span class='charSpecial'>\“</span>Stai attenta, piccola rondine, queste vette che separano la Spagna dalla Francia possono essere pericolose.<span class='charSpecial'>\”</span>",
-            "Ma lei era una rondine coraggiosa.<br>Sapeva quando riposare sui rami e quando riprendere il volo.",
-            "Attravers<span class='charSpecial'>ò</span> i Pirenei con grazia e forza, giungendo finalmente dall<span class='charSpecial'>’</span>altra parte.",
-            "Incontr<span class='charSpecial'>ò</span> altri uccelli migratori lungo il viaggio e insieme volarono facendosi compagnia gli uni all<span class='charSpecial'>’</span>altra.",
+            "Lo Stretto di Gibilterra era un passaggio stretto e ventoso <br>che separava l'Africa dall'Europa. Le correnti erano forti <br>e la rondine dovette lottare contro i venti impetuosi. <br>Attraversare lo Stretto era una delle parti pi<span class='charSpecial'>ù</span> difficili del suo viaggio, <br>ma la vista della terra dall<span class='charSpecial'>’</span>altra parte le dava la forza necessaria.",
+            "Con grande determinazione, <br>raggiunse finalmente le coste spagnole.",
+            "Dalla Spagna, la rondine vol<span class='charSpecial'>ò</span> verso i maestosi Monti Pirenei. Qui, i boschi erano <br>fitti e verdi, pieni di alberi alti e profumati. Era un luogo meraviglioso, ma il volo <br>tra le montagne non era semplice. Le correnti d'aria potevano <br>essere imprevedibili,<br>e a volte il cielo si copriva di nubi minacciose.",
+            "<span class='charSpecial'>\“</span>Stai attenta, piccola rondine, <br>queste vette che separano <br>la Spagna dalla Francia <br>possono essere pericolose.<span class='charSpecial'>\”</span>",
+            "Ma lei era una rondine coraggiosa. <br>Sapeva quando riposare sui rami <br>e quando riprendere il volo.",
+            "Attravers<span class='charSpecial'>ò</span> i Pirenei con grazia e forza, <br>giungendo finalmente dall<span class='charSpecial'>’</span>altra parte.",
+            "Incontr<span class='charSpecial'>ò</span> altri uccelli migratori lungo il viaggio <br>e insieme volarono facendosi compagnia gli uni all<span class='charSpecial'>’</span>altra.",
             "Finalmente, dopo molte settimane di viaggio, arriv<span class='charSpecial'>ò</span> in Italia.",
-            "La rondine trov<span class='charSpecial'>ò</span> un bel paesino vicino a un lago, dove decise di costruire il suo nuovo nido. Qui, i cieli erano azzurri e l'aria fresca. Ogni giorno, si nutriva di insetti e cinguettava felice.",
-            "<span class='charSpecial'>\“</span>La rondine ha affrontato tanti pericoli e ha attraversato luoghi magnifici.<br>Ora è giunta sana e salva in Italia, pronta per una nuova avventura. E chissà, forse un giorno volerà di nuovo verso il Kenya, portando con sé i ricordi di questo meraviglioso viaggio.<span class='charSpecial'>\”</span>",
-            "E cos<span class='charSpecial'>ì</span> termina il viaggio della rondine coraggiosa. Il suo spirito avventuroso continuerà a ispirare tutti coloro che amano esplorare il mondo."
+            "La rondine trov<span class='charSpecial'>ò</span> un bel paesino vicino a un lago, <br>dove decise di costruire il suo nuovo nido. <br>Qui, i cieli erano azzurri e l'aria fresca. <br>Ogni giorno, si nutriva di insetti e cinguettava felice.",
+            "<span class='charSpecial'>\“</span>La rondine ha affrontato tanti pericoli e ha attraversato luoghi magnifici.<br>Ora è giunta sana e salva in Italia, pronta per una nuova avventura. <br>E chissà, forse un giorno volerà di nuovo verso il Kenya, <br>portando con sé i ricordi di questo meraviglioso viaggio.<span class='charSpecial'>\”</span>",
+            "E cos<span class='charSpecial'>ì</span> termina il viaggio della rondine coraggiosa. <br>Il suo spirito avventuroso continuerà a ispirare <br>tutti coloro che amano esplorare il mondo."
         ];
 }
 
